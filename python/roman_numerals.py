896f80bbecd23403eb4d755b4687cca38e234e6d
Justin <jjpeterson90@hotmail.com>
2022-05-25 15:44:02 -0500

    complete

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,29 @@
 def to_roman(num):
-    # write your code here!
+    roman_result = ''
+    roman_to_arabic = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    }
+
+    while num > 0:
+        for key in roman_to_arabic:
+            if num / roman_to_arabic[key] >= 1:
+                roman_result += key
+                num -= roman_to_arabic[key]
+                break;
+                
+    return roman_result
+
+
+#print(to_roman(27))
